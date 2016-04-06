ef7dfec7eea6260cf7f86455f8cf30219e392f9d
Leonidas Papadopoulos <lewn.papadopoulos@gmail.com>
2016-04-07 01:28:21 +0300

    Modify MapWorker to handle tasks linearly
    Modify MapWorker to handle its tasks more linearly.

--- src/MapWorker.java
+++ src/MapWorker.java
@@ -15,22 +15,33 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
-public class MapWorker extends Worker {
+public class MapWorker extends Thread {
 
-	private List<Checkin> checkins; 	//adeio pros to parwn
-	double minLongitude, maxLongitude, minLatitude, maxLatitude; //suntetagmenes
+	private List<Checkin> checkins;
+	private Worker worker;
+	private double minLongitude, maxLongitude, minLatitude, maxLatitude;
+	
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://83.212.117.76:3306/ds_systems_2016?user=omada80&password=omada80db";
 	
-	
-	public MapWorker() {
-		checkins = new ArrayList<Checkin>();
-		//start server, get data, put in a list
-		openServer();
-		
-		
+	public MapWorker(Worker worker) {
+		this.worker = worker;
+		checkins = new ArrayList<Checkin>();	
 	}
 	
+	@Override
+	public void run() {
+		ServerSocket s = openServer();
+		Connection c = connectToDatabase(s);
+		doQuery(c);
+		closeDatabaseConnection(c);
+		closeServer(s);
+		map();
+		worker.notifyMasterOfCompletion();
+		sendToReducers(null);
+	}
+
+
 	public Map<String, Long> map() {
 		Stream<Checkin> stream = checkins.stream().parallel().distinct().filter(p -> p.getUrl()!=null);
 		Map<String, Long> map = stream
@@ -40,7 +51,8 @@ public class MapWorker extends Worker {
 		return map;
 	}
 	
-	public void notifyMaster() {
+	@SuppressWarnings("unused")
+	private void notifyMaster() {
 		
 	}
 	
@@ -48,107 +60,118 @@ public class MapWorker extends Worker {
 		
 	}
 	
-	private void openServer() {
-		ServerSocket sSocket = null;
-		Socket connection = null;
-		
+	private ServerSocket openServer() {
+		ServerSocket serverSocket = null;
 		try{
-			sSocket = new ServerSocket(1234);
-			while(true){
-				connection = sSocket.accept();
-				ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
-				ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
-				out.writeObject("Connection Successful.");
-				out.flush();
-				try{
-					minLatitude = (double) in.readObject();
-					minLongitude = (double) in.readObject();
-					maxLatitude = (double) in.readObject();
-					maxLongitude = (double) in.readObject();	
-
-					connectToDatabase();
-				}
-				catch(ClassNotFoundException cnfe){
-					System.err.println("Data received in unknown format. ");
-					cnfe.printStackTrace();
-				}
-				in.close();
-				out.close();
-				connection.close();
-			}
+			serverSocket = new ServerSocket(1234);
 		}
 		catch(IOException ioe){
 			ioe.printStackTrace();
 		}
-		finally{
-			try{
-				sSocket.close();
-			}catch(IOException ioe){
-				ioe.printStackTrace();
-			}
+		return serverSocket;
+	}
+	
+	private void closeServer(ServerSocket serverSocket){
+		try{
+			serverSocket.close();
+		}catch(IOException ioe){
+			ioe.printStackTrace();
 		}
 	}
 	
-	private void connectToDatabase(){
-		Connection conn = null;
-		Statement stmt = null;
+ 	private Connection connectToDatabase(ServerSocket s){
+		Connection connection = null;
 		try{
 			Class.forName(JDBC_DRIVER);
 			System.out.println("Connecting to database...");
-			conn = DriverManager.getConnection(DB_URL);
-			System.out.println("Creating statement...");
-			stmt = conn.createStatement();
-			doQuery(stmt); // executes query to database.
-			
-			
-			stmt.close();
-			conn.close();
-			
-		}catch(SQLException se){
-			se.printStackTrace();
-		}catch(Exception e){
+			connection = DriverManager.getConnection(DB_URL);
+			System.out.println("Connected to database...");
+		}
+		catch(ClassNotFoundException e){
+			System.err.println("Database class wasn't found.");
 			e.printStackTrace();
-		}finally{
+		}
+		catch (SQLException e){
+			System.err.println("An SQL exception was thrown.");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+ 	
+ 	private void closeDatabaseConnection(Connection c){
+ 		try {
+			c.close();
+		} 
+ 		catch (SQLException sQLException) {
+ 			sQLException.printStackTrace();
+		}
+ 	}
+ 	
+	@SuppressWarnings("unused")
+	private void getBounds(ServerSocket sSocket) throws IOException {
+		Socket connection = null;
+		
+		while(true){
+			connection = sSocket.accept();
+			ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+			ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
+			out.writeObject("Connection Successful.");
+			out.flush();
+
 			try{
-				if(stmt!=null)
-					stmt.close();
-		    }catch(SQLException se2){}// nothing we can do
-		    try{
-		      if(conn!=null)
-		    	  conn.close();
-		    }catch(SQLException se){
-		    	se.printStackTrace();
-		    }//end finally try
-		    map();
-		}//end try
-	}
-	
-	private void doQuery(Statement statement) throws SQLException{
-		int cores = Runtime.getRuntime().availableProcessors();
-		System.out.println(cores);
-		String sql =String.format("Select POI, photos"
-				+ " from ds_systems_2016.checkins "
-				+ "where (latitude between %f and %f) and (longitude between %f and %f);"
-				,minLatitude, maxLatitude, minLongitude, maxLongitude);
-		
-		ResultSet rs = statement.executeQuery(sql);
-		while(rs.next()){
-			String POI = rs.getString("POI");
-			String url = rs.getString("photos");
-			checkins.add(new Checkin(POI, url.equals("Not exists")? null: url));
+				minLatitude = (double) in.readObject();
+				minLongitude = (double) in.readObject();
+				maxLatitude = (double) in.readObject();
+				maxLongitude = (double) in.readObject();	
+			}
+			catch(ClassNotFoundException cnfe){
+				System.err.println("Data received in unknown format. ");
+				cnfe.printStackTrace();
+			}
+			in.close();
+			out.close();
 		}
-		rs.close();
 	}
 	
-	private static class myThread implements Runnable{
-		
-		
-		@Override
-		public void run() {
+	private void doQuery(Connection connection){
+		Statement stmt = null;
+		ResultSet rs = null;
+		try{
+			System.out.println("Creating statement...");
+			stmt = connection.createStatement();
+			System.out.print("Statement is created");
 			
+			int cores = Runtime.getRuntime().availableProcessors();
+			System.out.println(cores);
+			
+			String query =String.format("Select POI, photos"
+					+ " from ds_systems_2016.checkins "
+					+ "where (latitude between %f and %f) and (longitude between %f and %f);"
+					,minLatitude, maxLatitude, minLongitude, maxLongitude);
+			
+			rs = stmt.executeQuery(query);
+			
+			// Add query results to the map
+			while(rs.next()){
+				String POI = rs.getString("POI");
+				String url = rs.getString("photos");
+				checkins.add(new Checkin(POI, url.equals("Not exists")? null: url));
+			}
+		}
+		catch(SQLException se ){
+			se.printStackTrace();
+		}
+		finally {
+			try{
+				if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+			}
+			catch (SQLException sqlException){
+				sqlException.printStackTrace();
+			}
 			
 		}
-		
 	}
-	
 }
