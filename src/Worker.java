8b0dfc970c9934a516a79811c0935873600b084a
Leonidas Papadopoulos <lewn.papadopoulos@gmail.com>
2016-04-07 01:27:30 +0300

    Modify Worker to handle Runnables
    Modify Worker to handle Runnables.

--- src/Worker.java
+++ src/Worker.java
@@ -1,15 +1,24 @@
+import java.net.Socket;
 
 public class Worker {
+	private Runnable job;
+	private Socket socket;
 	
 	public Worker() {
-		
+		//TODO implement socket
 	}
 	
-	public void initialize() {
-
+	public void setJob(Runnable job){
+		this.job = job;
 	}
 	
-	public void waitForTasksThread() {
+	public void startWorking() {
+		job.run();
+	}
+	public void notifyMasterOfCompletion(){
 		
-	}	
+	}
+	public Socket getSocket(){
+		return socket;
+	}
 }
